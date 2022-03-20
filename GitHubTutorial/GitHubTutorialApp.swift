a4d05844a93cbe6fa63e75d27a386a3d921b04c7
Arvid64 <xylofoto@gmail.com>
2022-03-20 13:07:23 +0100

    Initial Commit

---
+++ GitHubTutorial/GitHubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTutorialApp.swift
+//  GitHubTutorial
+//
+//  Created by Arvid Ernst on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
