a1e7109e9d6c186ba46e2fbb67212d200d8a6a9b
Arvid64 <xylofoto@gmail.com>
2022-03-20 13:29:56 +0100

    create second view

---
+++ GitHubTutorial/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubTutorial
+//
+//  Created by Arvid Ernst on 20/03/2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
